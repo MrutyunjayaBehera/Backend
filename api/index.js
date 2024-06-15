45ee40499e3f63fc13c1bf0b8480e9f4b4803f18
MrutyunjayaBehera <mrutyunjaya.9029@gmail.com>
2024-06-15 19:16:44 +0530

    supabase auth login/signup integrated

--- api/index.js
+++ api/index.js
@@ -1,7 +1,12 @@
 const Axios = require('axios').default;
 const express = require('express');
+require('dotenv').config();
+const { supabase } = require('../services/supabase');
 const app = express();
 const port = 3000;
+
+// Middleware to parse JSON body
+app.use(express.json());
 
 // process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
 
@@ -18,7 +23,6 @@ app.get('/', (req, res) => {
 });
 
 app.get("/search", async (req, res) => {
-	console.log('req:: ', req);
 	const response = await request.get('/search', {
 		params: {
 			q: req.query.q
@@ -26,6 +30,32 @@ app.get("/search", async (req, res) => {
 	});
 	res.send(response.data);
 });
+
+
+app.post("/signup", async (req, res) => {
+	const { email = '', password = '' } = req.body || {};
+
+	const { data, error } = await supabase.auth.signUp({
+		email: email,
+		password: password
+		// password: 'bwLQFjBPtSGVbGcMFbwq'
+  	})
+  
+	res.send(data);  
+});
+
+app.post("/login", async (req, res) => {
+
+	const { email = '', password = '' } = req.body || {};
+	
+	const { data, error } = await supabase.auth.signInWithPassword({
+		email: email,
+		password: password
+  	})
+  
+	res.send(data);  
+});
+
 
 app.listen(port, () => {
 	console.log(`Example app listening at http://localhost:${port}`);
