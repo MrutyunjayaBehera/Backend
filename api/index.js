a77b976389ac3de6767f7de1376fa660caedb422
MrutyunjayaBehera <mrutyunjaya.9029@gmail.com>
2024-07-26 20:15:54 +0530

    host url fix

--- api/index.js
+++ api/index.js
@@ -101,7 +101,7 @@ const bot = new TelegramBot(token, {polling: true});
 //   bot.sendMessage(chatId, resp);
 // });
 
-const webhookUrl = `https://${process.env.VERCEL_DEPLOYED_URL}/api/telegram-bot`;
+const webhookUrl = `https://${process.env.VERCEL_DEPLOYED_URL}/api/index.js`;
 bot.setWebHook(webhookUrl);
 
 const dictRequest = Axios.create({
